80e25e47adb791c053963265f97993feb66d7156
João Vitor Bonadiman <18357636+jbonadiman@users.noreply.github.com>
2024-02-12 23:56:23 +0000

    replaces the theme music

--- nodes/autoload/music_player.gd
+++ nodes/autoload/music_player.gd
@@ -1,16 +1,16 @@
 extends AudioStreamPlayer
 
-@onready var timer: Timer = %Timer
-
-
-func _ready() -> void:
-	finished.connect(_on_finished)
-	timer.timeout.connect(_on_timer_timeout)
-
-
-func _on_finished() -> void:
-	timer.start()
-
-
-func _on_timer_timeout() -> void:
-	play()
+#@onready var timer: Timer = %IntervalTimer
+#
+#
+#func _ready() -> void:
+	#finished.connect(_on_finished)
+	#timer.timeout.connect(_on_timer_timeout)
+#
+#
+#func _on_finished() -> void:
+	#timer.start()
+#
+#
+#func _on_timer_timeout() -> void:
+	#play()
